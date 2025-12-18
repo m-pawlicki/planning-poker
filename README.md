354328eb496f9dde9104d5d96f978c6a13621ae5
Micah Pawlicki <mich.pawlicki@gmail.com>
2025-12-18 10:10:00 -0600

    initial commit

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
-# planning-poker
-
+# Planning Poker
 Planning Poker is an application where group deicision-making about the difficulty of a given topic is gamified via picking cards.
---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+//import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div className="card-container">
+      <div className="card">
+        <div className="card-front"></div>
+        <div className="card-back"><h1>2</h1></div>
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default App

4dacbdd173bd1491aeef0a1a86d17e9900d49dbd
Micah Pawlicki <mich.pawlicki@gmail.com>
2025-12-23 14:33:14 -0600

    cards

--- src/App.tsx
+++ src/App.tsx
@@ -4,41 +4,46 @@ import './App.css'
 function Card({value, onCardClick}) {
     return (
     <>
-    <div className="card-container" id="card-containero" onClick={onCardClick}>
-      <div className="card">
-        <div className="card-front"></div>
-        <div className="card-back"><h1>{value}</h1></div>
+    <div className="card-container" id="container">
+      <div className="card" id="card">
+        <div className="card-front" id="back"></div>
+        <div className="card-back" id="face" onClick={onCardClick}><h1>{value}</h1></div>
       </div>
     </div>
     </>
   )
 }
 
+function Hand() {
+  return (
+    <>
+      <div className="card-hand">
+        <Card value={0} onCardClick={() => handleClick()} />
+        <Card value={1} onCardClick={() => handleClick()} />
+        <Card value={2} onCardClick={() => handleClick()} />
+        <Card value={3} onCardClick={() => handleClick()} />
+        <Card value={5} onCardClick={() => handleClick()} />
+        <Card value={8} onCardClick={() => handleClick()} />
+        <Card value={13} onCardClick={() => handleClick()} />
+        <Card value={"?"} onCardClick={() => handleClick()} />
+        <Card value={"∞"} onCardClick={() => handleClick()} />
+        <Card value={"☕"} onCardClick={() => handleClick()} />
+      </div>
+    </>
+  )
+}
+
 
 function handleClick() {
-  const cardElement = document.getElementById("card-container");
-  cardElement?.classList.toggle("card-pause");
+  const cardElement = document.getElementById("container");
+  cardElement?.classList.toggle("card-choose");
 }
 
 export default function App() {
 
   return (
     <>
-    <div className="card-hand">
-      <Card value={0} onCardClick={() => handleClick} />
-      <Card value={1} onCardClick={() => handleClick} />
-      <Card value={2} onCardClick={() => handleClick} />
-      <Card value={3} onCardClick={() => handleClick} />
-      <Card value={5} onCardClick={() => handleClick} />
-      <Card value={8} onCardClick={() => handleClick} />
-      <Card value={13} onCardClick={() => handleClick} />
-      <Card value={20} onCardClick={() => handleClick} />
-      <Card value={40} onCardClick={() => handleClick} />
-      <Card value={100} onCardClick={() => handleClick} />
-      <Card value={"?"} onCardClick={() => handleClick} />
-      <Card value={"∞"} onCardClick={() => handleClick} />
-      <Card value={"☕"} onCardClick={() => handleClick} />
-    </div>
+    <Hand />
     </>
   )
 }

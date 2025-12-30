5202c1670d6ded85399499165a8699f50afac733
Micah Pawlicki <mich.pawlicki@gmail.com>
2025-12-30 03:19:33 -0600

    changed card animations

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,46 @@
 import { useState } from 'react';
 import './App.css'
 
-function Card({value, onCardClicked}) {
-    return (
+// A single card
+function Card({value}) {
+  return (
     <>
     <div className="card-container">
       <div className="card">
-        <div className="card-front"></div>
-        <div className="card-back" id={value} onClick={onCardClicked}><h1>{value}</h1></div>
+        <input type="radio" name="card" id={value} value={value}></input>
+        <label className="card-face" htmlFor={value}>{value}</label>
       </div>
     </div>
     </>
   )
+
 }
 
+// User's hand of cards
 function Hand() {
-
   return (
     <>
       <div className="card-hand">
-        <Card value={"0"} onCardClicked={() => handleClick("0")} />
-        <Card value={"1"} onCardClicked={() => handleClick("1")} />
-        <Card value={"2"} onCardClicked={() => handleClick("2")} />
-        <Card value={"3"} onCardClicked={() => handleClick("3")} />
-        <Card value={"5"} onCardClicked={() => handleClick("5")} />
-        <Card value={"8"} onCardClicked={() => handleClick("8")} />
-        <Card value={"13"} onCardClicked={() => handleClick("13")} />
-        <Card value={"?"} onCardClicked={() => handleClick("?")} />
-        <Card value={"☕"} onCardClicked={() => handleClick("☕")} />
+        <Card value={"0"} />
+        <Card value={"1"} />
+        <Card value={"2"} />
+        <Card value={"3"} />
+        <Card value={"5"} />
+        <Card value={"8"} />
+        <Card value={"13"} />
+        <Card value={"?"} />
+        <Card value={"☕"} />
       </div>
     </>
   )
 }
 
-
-function handleClick(value: string) {
-
-  const cardElement = document.getElementById(value);
-  cardElement?.classList.toggle("card-pick");
-
+// Shows cards picked from users, initially face down
+function Table() {
+  return (
+    <>
+    </>
+  )
 }
 
 export default function App() {

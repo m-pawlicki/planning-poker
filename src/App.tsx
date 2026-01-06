522a15f308b1989cc8d93154b6d293bd99d8c7ff
Micah Pawlicki <mich.pawlicki@gmail.com>
2026-01-06 11:23:04 -0600

    overhauled layout

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function Card({value}) {
 
 }
 
-// User's hand of cards
+// A user's hand of cards
 function Hand() {
   return (
     <>
@@ -31,17 +31,70 @@ function Hand() {
         <Card value={"?"} />
         <Card value={"☕"} />
       </div>
+      <div>
+        <button className="button">Submit</button><button className="button">Skip</button>
+      </div>
     </>
   )
 }
 
-// Shows cards picked from users, initially face down
+// Visual representation of cards picked by users and the current story to vote upon
 function Table() {
   return (
     <>
+    <h2>Current Story:</h2>
+    <div className="table-container">
       <svg width="500" height="250">
         <rect width="500" height="250" rx="15" ry="15" fill="sienna" stroke="saddlebrown" strokeWidth="5" />
-      </svg> 
+      </svg>
+    </div>
+    <div>
+      <button className="button">Reveal Cards</button>
+    </div>
+    </>
+  )
+}
+
+// Header
+function Header() {
+  return (
+    <>
+    <h1>Planning Poker</h1>
+    </>
+  )
+}
+
+// Footer
+function Footer() {
+  return (
+    <>
+    <sub>fneet (c) 2026</sub>
+    </>
+  )
+}
+
+// User list, current voting status, and how they voted (when revealed)
+function Users() {
+  return (
+    <>
+    <h2>Users</h2>
+    <h3>Currently Voting</h3>
+    <br />
+    <h3>Voted</h3>
+    < br />
+    <h3>Skipped</h3>
+    </>
+  )
+}
+
+// Shows the list of stories, vote status of stories, and comments tied to the current story
+function Info() {
+  return (
+    <>
+    <h2>Info</h2>
+    <h3>Stories</h3>
+    <br />
+    <h3>Comments</h3>
     </>
   )
 }
@@ -52,12 +105,26 @@ export default function App() {
 
   return (
     <>
-        <div className="table">
+      <div className="parent">
+        <div className="div-head">
+          <Header />
+        </div>
+        <div className="div-users">
+          <Users />
+        </div>
+        <div className="div-table">
           <Table />
         </div>
-        <div className="hand">
+        <div className="div-cards">
           <Hand />
         </div>
+        <div className="div-info">
+          <Info />
+        </div>
+        <div className="div-foot">
+          <Footer />
+        </div>
+      </div>
     </>
   )
 }
